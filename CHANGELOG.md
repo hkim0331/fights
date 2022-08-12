2391499f5a56c9dac891969b27aab7c00ff1810c
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-08-12 17:15:25 +0900

    insert-persons year

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,15 @@
 # Change Log
 
 ## Unreleased
+- persons: country_short_blue, country_short_white を入れた方が良くないか？
 
 ## 0.2.0-SNAPSHOT
 - src/fights/db.clj create tables. `persons`, `contests` and `competitions`.
 - test/fights/db_test.clj
 - src/fights/competitor.clj
 - src/fights/core.clj
-- can insert-person
-
+- (insert-person-id id_competition)
+- (insert-pserons year)
 
 ## 0.1.0 - 2022-08-12
 - project (re)started.
--- src/fights/persons.clj
+++ src/fights/persons.clj
@@ -1,7 +1,7 @@
 (ns fights.persons
- (:require
-  [fights.db :as db]
-  [hato.client :as hc]))
+  (:require
+   [fights.db :as db]
+   [hato.client :as hc]))
 
 (def ^:private base "https://data.ijf.org/api/get_json")
 
@@ -17,18 +17,20 @@
 ;;(get-contests 2289)
 
 (defn insert-person-one
-  [{:keys [id_person_blue given_name_blue family_name_blue
-           id_person_white given_name_white family_name_white]}]
-  (println [id_person_blue given_name_blue family_name_blue
-            id_person_white given_name_white family_name_white])
+  [{:keys [id_person_blue given_name_blue family_name_blue country_short_blue
+           id_person_white given_name_white family_name_white country_short_white]}]
+  (println [id_person_blue given_name_blue family_name_blue country_short_blue
+            id_person_white given_name_white family_name_white country_short_white])
   (db/insert-person  {:id_person   id_person_blue
                       :family_name family_name_blue
-                      :given_name  given_name_blue})
+                      :given_name  given_name_blue
+                      :country     country_short_blue})
   (db/insert-person  {:id_person   id_person_white
                       :family_name family_name_white
-                      :given_name  given_name_white}))
+                      :given_name  given_name_white
+                      :country     country_short_white}))
 
-(defn insert-person
+(defn insert-persons-id
   "id_competion から
    id_person family_name given_name をとってくる。"
   [id_competition]
@@ -36,5 +38,9 @@
   (doseq [competition (get-contests id_competition)]
     (insert-person-one competition)))
 
-(insert-person 2389)
- 
+;;(insert-persons-id 2389)
+
+(defn insert-persons
+  [year]
+  (doseq [{:keys [id_competition]} (db/competition-id-year year)]
+    (insert-persons-id id_competition)))
